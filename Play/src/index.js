ebba070138fe58786404a5484e89da20a4875548
vivek-panchal <vivek.nsut@gmail.com>
2024-02-03 14:01:25 +0530

    utils and app.js code

--- Play/src/index.js
+++ Play/src/index.js
@@ -7,8 +7,15 @@ dotenv.config({
 })
 
 
-
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log( `Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed !!!", err);
+})
 
 
 
@@ -18,7 +25,7 @@ connectDB()
 
 
 
-
+/*************** Another way to connect DB *******************/
 /*
 import express from "express"
 const app = express()
