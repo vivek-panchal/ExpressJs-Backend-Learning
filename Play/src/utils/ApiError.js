11d5d342d43ead4a60e0a4d28ca1abe462c5a924
vivek-panchal <vivek.nsut@gmail.com>
2024-02-04 14:02:04 +0530

    cloudinary setup & multer

--- Play/src/utils/ApiError.js
+++ Play/src/utils/ApiError.js
@@ -12,7 +12,7 @@ class ApiError extends Error {
         this.success = false;
         this.errors = errors
 
-        if (statck) {
+        if (stack) {
             this.stack = statck
         } else{
             Error.captureStackTrace(this, this.constructor)
